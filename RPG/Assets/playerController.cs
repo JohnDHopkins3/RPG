97b1088996cfdd463189e744aaf6ab3fd939a734
JOHNDHOPKINS <45177624+JOHNDHOPKINS@users.noreply.github.com>
2019-05-02 13:51:15 -0400

    movement added

---
+++ RPG/Assets/playerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(playerMotor))]
+public class playerController : MonoBehaviour
+{
+
+    [SerializeField]
+    private float speed = 5f;
+
+    private playerMotor motor;
+
+    private void Start()
+    {
+        motor = GetComponent<playerMotor>();
+    }
+
+    private void Update()
+    {
+        //calculate movement velocity as 3D vector
+        float _XMovement = Input.GetAxisRaw("Horizontal");
+        float _ZMovement = Input.GetAxisRaw("Vertical");
+
+        Vector3 _movHorizontal = transform.right * _XMovement;
+        Vector3 _movVertical = transform.forward * _ZMovement;
+
+        //final movement vector
+        Vector3 _velocity = (_movHorizontal + _movVertical).normalized * speed;
+
+        //apply movement
+        motor.Move(_velocity);
+
+    }
+}
---
+++ RPG/Assets/playerMotor.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class playerMotor : MonoBehaviour
+{
+    private Vector3 velocity = Vector3.zero;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    //gets a movement method
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    //Run every phisics iteration
+    private void FixedUpdate()
+    {
+        PerformMovement();
+    }
+
+    //Perform movement based on velocity variable
+    void PerformMovement()
+    {
+        if (velocity !=Vector3.zero)
+        {
+            rb.MovePosition(rb.position+velocity*Time.deltaTime);
+        }
+    }
+
+}
