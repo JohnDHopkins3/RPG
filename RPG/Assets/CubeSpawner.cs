a571c564eb1473e119f6f80d7c3ec80d1a1be223
JOHNDHOPKINS <45177624+JOHNDHOPKINS@users.noreply.github.com>
2019-05-13 13:54:56 -0400

    collision and cubes

---
+++ RPG/Assets/CubeSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour
+{
+    public GameObject gameObject;
+    
+    public Vector3 sizeIncrease=new Vector3(1,1,1);
+    public Vector3 offset;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Comma))
+        {
+            gameObject.transform.localScale += sizeIncrease;
+        }
+        if (Input.GetKeyDown(KeyCode.Period))
+        {
+            gameObject.transform.localScale -= sizeIncrease;
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Instantiate(gameObject, transform.position+offset, Quaternion.identity);
+        }
+        gameObject.transform.localScale = sizeIncrease;
+    }
+}
